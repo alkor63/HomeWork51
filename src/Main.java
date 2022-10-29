a5ba5ba4e0f4e32b5bf87e00ad11597265054488
alkor63 <alkor@medi-spa.ru>
2022-10-29 14:24:30 +0300

    Сделал доп ДЗ 4,5

--- src/Main.java
+++ src/Main.java
@@ -38,15 +38,14 @@ public class Main {
 // Одобрить ли кредит заёмщику?
         age = 25;
         salary = 60000;
-        int creditTerm = 12;                // срок кредитования, мес
         int wantedSum = 330000;             // запрашиваемая сумма кредита
         double monthlyPayment;              // ежемесячный платеж по кредиту
         int maxMonthlyPay = salary / 2;     // максимально допустимый месячный платеж
-        double creditRate = 0.1;            // базовая ставка кредита
-        if (salary > 80000) creditRate = creditRate - 0.007;
-        if (age < 30) creditRate = creditRate + 0.005;
-        if (age < 23) creditRate = creditRate + 0.005;
-        monthlyPayment = wantedSum * (1 + creditRate) / creditTerm;
+        double creditRate = 10.;            // базовая ставка кредита, %
+        if (salary > 80000) creditRate = creditRate - 0.7;
+        if (age < 30) creditRate = creditRate + 0.5;
+        if (age < 23) creditRate = creditRate + 0.5;
+        monthlyPayment = wantedSum * (1 + creditRate/100) / 12;
         System.out.println("Максимальный платеж при ЗП "+salary+" равен "+maxMonthlyPay+" рублей");
         System.out.println("Платеж по кредиту "+ monthlyPayment +" рублей");
         if (monthlyPayment > maxMonthlyPay) System.out.println("В кредите отказано");
